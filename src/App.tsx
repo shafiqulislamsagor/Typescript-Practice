e7bb69c34f144c5bf4709230a33e86869ace6474
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-07-11 11:46:46 +0600

    useReducer hooks

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Greet from "./components/Greet";
 import Input from "./components/Input";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Counter from "./components/state/Counter";
 import LoggedIn from "./components/state/LoggedIn";
 import { User } from "./components/state/User";
 import Status from "./components/Status";
@@ -45,9 +46,10 @@ function App() {
         <Form name={named} handleFrom={FormHandleClick}/>
         <Container styles={{background:"black" , padding:'4px 5px' , color:'white'}} Twd="p-2 border"/>
         <LoggedIn/>
+        <User name="sagor" email="sagor@example.com"/>
       </div>
       <div>
-        <User name="sagor" email="sagor@example.com"/>
+        <Counter/>
       </div>
     </>
   );
