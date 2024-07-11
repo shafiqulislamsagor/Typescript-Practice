f05189b74175ab7eef03fee9e8fb80a6b27732fa
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-07-11 11:20:17 +0600

    useState type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,19 +6,20 @@ type AuthUser = {
 }
 
 export const User =({name , email}:AuthUser) =>{
-    const [user , setUser] = useState<null | AuthUser>(null)
+    // const [user , setUser] = useState<null | AuthUser>(null)
+    const [user , setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = ():void =>{
         setUser({name, email})
     };
-    const handleLogout = ():void =>{
-        setUser(null)
-    };
+    // const handleLogout = ():void =>{
+    //     setUser(null)
+    // };
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
